0a0d545c50cc5e2ad1bd7f4f6b0cdc35888f39ed
Guy Blumenthal <guy.blumenthal@outlook.com>
2021-12-16 16:50:17 -0500

    Added animations and flex box

--- src/components/Cell.js
+++ src/components/Cell.js
@@ -1,16 +1,21 @@
-import black from "../assets/black.png";
 import IconSelector from "../IconSelector";
 
 function Cell(props) {
   const selected = props.solved || props.val < 0;
   const number = Math.abs(props.val);
+  const styling = {};
+  if (selected) {
+    styling["backgroundImage"] = IconSelector(number);
+  }
+  console.log(styling);
   return (
-    <img
-      el={number}
+    <div
       className={`cell ${selected ? "selected" : ""}`}
-      src={selected ? IconSelector(number) : black}
       onClick={props.select}
-    />
+      style={styling}
+    >
+      <img src={selected ? IconSelector(number) : ""} />
+    </div>
   );
 }
 
