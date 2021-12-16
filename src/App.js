b2ff6dabfe4b0cd64c84429899b6e4fe16719329
Guy Blumenthal <guy.blumenthal@outlook.com>
2021-12-16 15:11:57 -0500

    Initial commit with prettier formatting

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
-import { useState } from "react"
-import './App.css';
+import { useState } from "react";
+import "./App.css";
 import Cell from "./components/Cell";
 
 function generateBoard(n) {
@@ -7,7 +7,7 @@ function generateBoard(n) {
 
   const numScrambles = 1000000;
 
-  const board = [...Array(gridSize).keys()].map((i) => Math.floor(i/2) + 1)
+  const board = [...Array(gridSize).keys()].map((i) => Math.floor(i / 2) + 1);
 
   for (let scramble = 0; scramble < numScrambles; scramble++) {
     const index1 = Math.floor(Math.random() * gridSize);
@@ -18,55 +18,53 @@ function generateBoard(n) {
     board[index2] = v1;
   }
 
-  return board
-
+  return board;
 }
 
 function select(index, board, setBoard, solved, setSolved) {
+  const numSelected = board.map((i) => i < 0).reduce((a, b) => a + b, 0);
+  const selectedVal = board[index];
 
-  const numSelected = board.map((i) => i < 0).reduce((a, b) => a + b, 0)
-  const selectedVal = board[index]
+  const selectable = selectedVal > 0 && !solved[selectedVal - 1];
 
-  const selectable = selectedVal > 0 && !solved[selectedVal - 1]
-  
   if (numSelected < 2 && selectable) {
-    const newBoard = [...board]
-    newBoard[index] = -board[index]
-    setBoard(newBoard)
+    const newBoard = [...board];
+    newBoard[index] = -board[index];
+    setBoard(newBoard);
     if (numSelected === 1) {
-      const [a, b] = newBoard.filter((e) => e < 0)
+      const [a, b] = newBoard.filter((e) => e < 0);
       if (a === b) {
-        const newSolved = [...solved]
-        newSolved[Math.abs(a) - 1] = true
-        setSolved(newSolved)
-        setBoard(newBoard.map(Math.abs))
+        const newSolved = [...solved];
+        newSolved[Math.abs(a) - 1] = true;
+        setSolved(newSolved);
+        setBoard(newBoard.map(Math.abs));
       } else {
         setTimeout(() => {
-          setBoard(newBoard.map(Math.abs))
-        }, 3000)
+          setBoard(newBoard.map(Math.abs));
+        }, 3000);
       }
     }
   }
 }
 
-
 function App() {
-  const size = 4
+  const size = 4;
 
-  const [board, setBoard] = useState(generateBoard(size))
-  const [solved, setSolved] = useState([...Array(size ** 2 / 2)].fill(false))
+  const [board, setBoard] = useState(generateBoard(size));
+  const [solved, setSolved] = useState([...Array(size ** 2 / 2)].fill(false));
 
-  return <div className="board">
-    {
-      board.map((val, index) => <Cell 
-        key={index} 
-        val={val} 
-        solved={solved[Math.abs(val) - 1]}
-        select={() => select(index, board, setBoard, solved, setSolved)}
-      />)
-    }
-  </div>
-
+  return (
+    <div className="board">
+      {board.map((val, index) => (
+        <Cell
+          key={index}
+          val={val}
+          solved={solved[Math.abs(val) - 1]}
+          select={() => select(index, board, setBoard, solved, setSolved)}
+        />
+      ))}
+    </div>
+  );
 }
 
 export default App;
--- src/IconSelector.js
+++ src/IconSelector.js
@@ -1,66 +1,66 @@
-import image1 from './assets/Cat Icons/photo (1).jpg'
-import image2 from './assets/Cat Icons/photo (2).jpg'
-import image3 from './assets/Cat Icons/photo (3).jpg'
-import image4 from './assets/Cat Icons/photo (4).jpg'
-import image5 from './assets/Cat Icons/photo (5).jpg'
-import image6 from './assets/Cat Icons/photo (6).jpg'
-import image7 from './assets/Cat Icons/photo (7).jpg'
-import image8 from './assets/Cat Icons/photo (8).jpg'
-import image9 from './assets/Cat Icons/photo (9).jpg'
-import image10 from './assets/Cat Icons/photo (10).jpg'
-import image11 from './assets/Cat Icons/photo (11).jpg'
-import image12 from './assets/Cat Icons/photo (12).jpg'
-import image13 from './assets/Cat Icons/photo (13).jpg'
-import image14 from './assets/Cat Icons/photo (14).jpg'
-import image15 from './assets/Cat Icons/photo (15).jpg'
-import image16 from './assets/Cat Icons/photo (16).jpg'
-import image17 from './assets/Cat Icons/photo (17).jpg'
-import image18 from './assets/Cat Icons/photo (18).jpg'
+import image1 from "./assets/Cat Icons/photo (1).jpg";
+import image2 from "./assets/Cat Icons/photo (2).jpg";
+import image3 from "./assets/Cat Icons/photo (3).jpg";
+import image4 from "./assets/Cat Icons/photo (4).jpg";
+import image5 from "./assets/Cat Icons/photo (5).jpg";
+import image6 from "./assets/Cat Icons/photo (6).jpg";
+import image7 from "./assets/Cat Icons/photo (7).jpg";
+import image8 from "./assets/Cat Icons/photo (8).jpg";
+import image9 from "./assets/Cat Icons/photo (9).jpg";
+import image10 from "./assets/Cat Icons/photo (10).jpg";
+import image11 from "./assets/Cat Icons/photo (11).jpg";
+import image12 from "./assets/Cat Icons/photo (12).jpg";
+import image13 from "./assets/Cat Icons/photo (13).jpg";
+import image14 from "./assets/Cat Icons/photo (14).jpg";
+import image15 from "./assets/Cat Icons/photo (15).jpg";
+import image16 from "./assets/Cat Icons/photo (16).jpg";
+import image17 from "./assets/Cat Icons/photo (17).jpg";
+import image18 from "./assets/Cat Icons/photo (18).jpg";
 
-import red from './assets/red.png'
+import red from "./assets/red.png";
 
 function IconSelector(index) {
-    index = Math.abs(index)
-    switch (index) {
-        case 1:
-            return image1;
-        case 2:
-            return image2;
-        case 3:
-            return image3;
-        case 4:
-            return image4;
-        case 5:
-            return image5;
-        case 6:
-            return image6;
-        case 7:
-            return image7;
-        case 8:
-            return image8;
-        case 9:
-            return image9;
-        case 10:
-            return image10;
-        case 11:
-            return image11;
-        case 12:
-            return image12;
-        case 13:
-            return image13;
-        case 14:
-            return image14;
-        case 15:
-            return image15;
-        case 16:
-            return image16;
-        case 17:
-            return image17;
-        case 18:
-            return image18;
-        default:
-            return red;
-    }
+  index = Math.abs(index);
+  switch (index) {
+    case 1:
+      return image1;
+    case 2:
+      return image2;
+    case 3:
+      return image3;
+    case 4:
+      return image4;
+    case 5:
+      return image5;
+    case 6:
+      return image6;
+    case 7:
+      return image7;
+    case 8:
+      return image8;
+    case 9:
+      return image9;
+    case 10:
+      return image10;
+    case 11:
+      return image11;
+    case 12:
+      return image12;
+    case 13:
+      return image13;
+    case 14:
+      return image14;
+    case 15:
+      return image15;
+    case 16:
+      return image16;
+    case 17:
+      return image17;
+    case 18:
+      return image18;
+    default:
+      return red;
+  }
 }
 
 export default IconSelector;
